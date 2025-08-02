9ca27cfed6c6769dd0967659d8e06ecc2441fc15
morteza <morteza.abdolahi2323@gmail.com>
2025-08-02 16:15:43 +0330

    storing uploaded files in directdmin host

--- pages/api/orders/orders.js
+++ pages/api/orders/orders.js
@@ -45,7 +45,7 @@ export default async function handler(req, res) {
       const buffer = Buffer.from(data, "base64");
 
       const fileName = `${uuidv4()}.${ext}`;
-      const filePath = path.join(process.cwd(), "public", "uploads", fileName);
+      const filePath = path.join("/home/x3drobotsazcom/domains/3drobotsaz.com/public_html/uploads", fileName);
       fs.writeFileSync(filePath, buffer);
 
       const fileUrl = `${process.env.BACKEND_URL}/uploads/${fileName}`;
